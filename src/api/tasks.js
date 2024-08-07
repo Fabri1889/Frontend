796defa77854dbbde008fd74913e4a79055c4411
Fabri1889 <131361629+Fabri1889@users.noreply.github.com>
2024-08-07 01:20:56 -0300

    Update tasks.js
    se comento el codigo

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,12 +1,17 @@
 import axios from "./axios";
 
+// Obtiene todas las tareas
 export const getTasksRequest = async () => axios.get("/tasks");
 
+// Crea una nueva tarea
 export const createTaskRequest = async (task) => axios.post("/tasks", task);
 
+// Actualiza una tarea existente por ID
 export const updateTaskRequest = async (id, task) =>
   axios.put(`/tasks/${id}`, task);
 
+// Elimina una tarea por ID
 export const deleteTaskRequest = async (id) => axios.delete(`/tasks/${id}`);
 
+// Obtiene una tarea específica por ID
 export const getTaskRequest = async (id) => axios.get(`/tasks/${id}`);
