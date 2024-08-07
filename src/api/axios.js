a41ea9dedf4f8d86c901d5d38d35c39b75150091
Fabri1889 <131361629+Fabri1889@users.noreply.github.com>
2024-08-07 01:20:43 -0300

    Update axios.js
    se comento el codigo

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { API_URL } from "../config";
 
+// Crea una instancia de axios con la URL base y las credenciales
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
