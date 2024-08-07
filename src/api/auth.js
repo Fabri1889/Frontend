64908bf18f403e6465f17f0c79a9b7cb5a062d78
Fabri1889 <131361629+Fabri1889@users.noreply.github.com>
2024-08-07 01:20:20 -0300

    Update auth.js
    se comento el codigo

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,11 @@
 import axios from "./axios";
 
+// Realiza una solicitud POST a /auth/register para registrar un nuevo usuario
 export const registerRequest = async (user) =>
   axios.post(`/auth/register`, user);
 
+// Realiza una solicitud POST a /auth/login para iniciar sesión
 export const loginRequest = async (user) => axios.post(`/auth/login`, user);
 
+// Realiza una solicitud GET a /auth/verify para verificar el token de autenticación
 export const verifyTokenRequest = async () => axios.get(`/auth/verify`);
